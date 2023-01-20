a8f05ac817204c39edef37b080e12fe0bae3c108
Eroxl <InfinityWaffles@outlook.com>
2023-01-19 19:59:10 -0800

    Setup index page

--- src/index.php
+++ src/index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Task Manager</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Task Manager</title>
+        <link rel="stylesheet" href="./index.css"></link>
+    </head>
+    <body>
+        <div class="navbar">
+            <h1>PHP Task Manager</h1>
+            <a href="./account/login.php">Login</a>
+        </div>
+    </body>
 </html>
